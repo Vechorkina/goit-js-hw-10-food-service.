a08de38c7d72324881cd250756d76d75ae7a1235
Vechorkina <artdeco.t@i.ua>
2020-12-22 12:10:28 +0200

    Deploy

---
+++ src/index.js
@@ -0,0 +1,60 @@
+// import './styles.css';
+
+//импорт данных
+import './styles.css';
+import menuItemTpl from './templates/menu-item.hbs';
+import menuItemArr from './menu.json';
+
+//создание верстки по шаблону
+function createMenuItem(menuItemArr) {
+  return menuItemTpl(menuItemArr);
+}
+
+const menuContainer = document.querySelector('.js-menu');
+const menuMarkup = createMenuItem(menuItemArr);
+menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
+
+//сохранение темы и положение чекбокса в Local Storage
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkbox = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+checkbox.addEventListener('click', onCheckboxClick);
+
+const savedData = localStorage.getItem('my-data');
+const parsedData = JSON.parse(savedData);
+
+function storageChecked() {
+  if (parsedData) {
+    checkbox.checked = parsedData.checked;
+  }
+}
+
+storageChecked();
+
+function storageTheme() {
+  if (parsedData) {
+    body.classList.add(parsedData.theme);
+  }
+}
+
+storageTheme();
+
+function onCheckboxClick() {
+  body.classList.toggle(Theme.DARK);
+
+  if (checkbox.checked) {
+    localStorage.setItem(
+      'my-data',
+      JSON.stringify({ theme: body.classList.value, checked: true }),
+    );
+  } else if (!checkbox.checked) {
+    localStorage.setItem(
+      'my-data',
+      JSON.stringify({ theme: body.classList.value, checked: false }),
+    );
+  }
+};
